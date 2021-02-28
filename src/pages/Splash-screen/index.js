3081aa8eefedb4c8acc7838e5e3292de2d0d322a
anggadewa <anggadewa2016@gmail.com>
2021-02-28 10:08:19 +0700

    layouting splash-screen

---
+++ src/pages/Splash-screen/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+import {ILLogo} from '../../assets/illustration';
+
+const Splash = () => {
+  return (
+    <View style={styles.pagwWrapper}>
+      <ILLogo />
+      <Text style={styles.textLogo}>My Doctor</Text>
+    </View>
+  );
+};
+
+export default Splash;
+const styles = StyleSheet.create({
+  pagwWrapper: {
+    backgroundColor: 'white',
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  textLogo: {
+    fontSize: 20,
+    fontWeight: '600',
+    color: '#112340',
+    marginTop: 20,
+  },
+});
